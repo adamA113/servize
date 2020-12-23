dd9bb659cecddce80d1ecd5f135a32c8dfb1b947
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-23 21:52:35 +0200

    create component to display list of booking

---
+++ client/src/components/booking/BookingList.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FontAwesome from 'react-fontawesome';
+
+export default function BookingList() {
+	
+    return (
+		
+ 
+
+    )
+}
