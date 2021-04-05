35beb3a5707de555380c2628e1cfd2e6483ab005
Yasir Tobbileh <yasur96@gmail.com>
2021-04-05 23:23:10 +0300

    feat/ add more detailed action types

--- client/src/actions/actionTypes.ts
+++ client/src/actions/actionTypes.ts
@@ -1,5 +1,19 @@
 export const STORE_USER = 'STORE_USER';
 export const USER_LOGIN = 'USER_LOGIN';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAIL = 'SIGNUP_FAIL';
+export const USER_LOADED_SUCCESS = 'USER_LOADED_SUCCESS';
+export const USER_LOADED_FAIL = 'USER_LOADED_FAIL';
+export const AUTHENTICATED_SUCCESS = 'AUTHENTICATED_SUCCESS';
+export const AUTHENTICATED_FAIL = 'AUTHENTICATED_FAIL';
+export const ACTIVATION_SUCCESS = 'ACTIVATION_SUCCESS';
+export const ACTIVATION_FAIL = 'ACTIVATION_FAIL';
+
+export const LOGOUT = 'LOGOUT';
+
 export const ADD_ROLE = 'ADD_ROLE';
 export const STORE_DATA = 'STORE_DATA';
 export const GET_PROV = 'GET_PROV';
@@ -21,7 +35,6 @@ export interface UserRole {
 export interface Userdata {
     type: typeof STORE_DATA,
     payload: User
-
 }
 
 export interface Providers {
@@ -47,6 +60,6 @@ export type User = UserIerface | null | string
 
 export type AllProv = ProvidersInt | null | object
 
-export type DispatchUserTypes = Userstore | Userdata | UserRole | Userlogin;
+export type DispatchUserTypes = Userstore | Userdata | UserRole | Userlogin | any;
 
 export type DispatchProvTypes = Providers ;
