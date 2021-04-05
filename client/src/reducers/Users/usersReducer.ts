759f99899c85b7fc543075f6909416b7aa754c50
Yasir Tobbileh <yasur96@gmail.com>
2021-04-05 23:24:23 +0300

    refactor/ fix userreducer

--- client/src/reducers/Users/usersReducer.ts
+++ client/src/reducers/Users/usersReducer.ts
@@ -1,28 +1,79 @@
-import { User, STORE_USER, STORE_DATA, ADD_ROLE, USER_LOGIN, DispatchUserTypes } from '../../actions/actionTypes';
+import {
+    User, STORE_USER, STORE_DATA, ADD_ROLE, USER_LOGIN, DispatchUserTypes,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    USER_LOADED_SUCCESS,
+    USER_LOADED_FAIL,
+    AUTHENTICATED_SUCCESS,
+    AUTHENTICATED_FAIL,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
+    LOGOUT
+} from '../../actions/actionTypes';
 
 export interface State {
-    user: User |any,
-    role: string |any,
-    // prov: {name:string, description:string, img: string}
+    user: User | any,
+    role: string | any,
+    access: string | any,
+    refresh: string | any,
+    isAuthenticated: any
 }
-export const initState: State = { user: null, role: null }
-// const initState = {
-//     user: null,
+export const initState: State = {
+    user: null,
+    role: null,
+    access: localStorage.getItem('access'),
+    refresh: localStorage.getItem('refresh'),
+    isAuthenticated: null,
+}  
 
-//     prov: [
-//         { name: "tech", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", img: "https://picsum.photos/id/0/200/300" },
-//         { name: "plumb", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", img: "https://picsum.photos/id/1015/200/300" }
-//     ]
-// }   
-// type A = ReturnType<typeof store>; 
 const usersReducer = (state: State = initState, action: DispatchUserTypes): State => {
     console.log("actiontype", action.type)
+
     switch (action.type) {
+        case USER_LOADED_SUCCESS:
+            return {
+                ...state,
+                user: action.payload
+            }
+        case USER_LOADED_FAIL:
+            return {
+                ...state,
+                user: null
+            }
+
+        case LOGIN_SUCCESS:
+            localStorage.setItem('access', action.payload.access);
+            localStorage.setItem('refresh', action.payload.refresh);
+            return {
+                ...state,
+                isAuthenticated: true,
+                access: action.payload.access,
+                refresh: action.payload.refresh
+            }
+
+        case SIGNUP_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: false
+            }
+        case LOGIN_FAIL:
+        case SIGNUP_FAIL:
+        case LOGOUT:
+            localStorage.removeItem('access');
+            localStorage.removeItem('refresh');
+            return {
+                ...state,
+                access: null,
+                refresh: null,
+                isAuthenticated: false,
+                user: null
+            }
+
         case STORE_USER:
-            // return { ...state, user: action.payload }
-            // return { ...state, user: [action.payload, ...state.user] }
-            return { ...state , user: action.payload }
-            
+            return { ...state, user: action.payload }
+
         case USER_LOGIN:
             return { ...state, user: action.payload }
 
@@ -35,29 +86,6 @@ const usersReducer = (state: State = initState, action: DispatchUserTypes): Stat
         default:
             return state
     }
-
-    // console.log("action", action);
-    // console.log("userstore", state);
-    // if (actionTypes.STORE_USER === 'STORE_USER') {
-    // try {
-    //     const serializedState = JSON.stringify({
-    //         ...state,
-    //         user: action.payload
-    //         // user: [ ...state.user, action.user]
-    //     });
-    //     localStorage.setItem("state", serializedState)
-    // }
-    // catch (e) {
-    //     console.log(e)
-    // }
-    // return {
-    //     ...state,
-    //     user: action.payload,
-    // image: action.payload.catImage
-    // user: [...state.user, action.user]
-    //     }
-    // }
-    // return state;
 }
 
 export default usersReducer;
