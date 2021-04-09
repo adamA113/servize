c2aaee21895637ab93f5b64a497ee17be3876a0a
Yasir Tobbileh <yasur96@gmail.com>
2021-04-09 20:27:37 +0300

    feat/ implement reset password form

--- client/src/components/Reset-password/Reset-password.tsx
+++ client/src/components/Reset-password/Reset-password.tsx
@@ -1,59 +1,43 @@
-
-// import { showSign } from '../actions/actions.js';
-// import './NewPassword.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { confirmResetPassword } from '../../actions/Users/usersActions';
 import { Link, Redirect } from 'react-router-dom';
-// import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
-import React from 'react';
-const axios = require('axios');
-// const $ = require('jquery');
+import './Reset-password.css';
+const ResetPassword = ({ match }) => {
+    const dispatch = useDispatch();
 
+    const [formData, setFormData] = useState({
+        newPassword: '',
+        confirmPassword: ''
+    });
+    const { newPassword, confirmPassword } = formData;
+    console.log(newPassword)
 
-const ResetPassword = () => {
+    const handleChange = (e: any) => setFormData({ ...formData, [e.target.id]: e.target.value });
+
+    const uid = match.params.uid;
+    const token = match.params.token;
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        // console.log("hi");
-        let input = $('#reset-form').serializeArray();
-        console.log(input);
-
-        let options = {
-            url: `http://localhost:3000/user/reset/:token`,
-            method: 'post',
-            // data: { email: input[0].value }
-        }
-
-        axios(options)
-            .then((results: any) => {
-                console.log(results);
-
-            })
-            .catch((err: any) => {
-                console.error("err===== =>", err);
-            })
+        dispatch(confirmResetPassword(uid, token, newPassword));
     }
 
     return (
-        <MDBContainer>
-            <MDBRow>
-                <MDBCol md="6">
-                    <form className="reset-form" id="reset-form" onSubmit={handleSubmit}>
-                        <p className="h4 text-center mb-4">Reset Password</p>
-                        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-                            New Password
-                        </label>
-                        <input type="password" id="defaultFormLoginEmailEx" className="form-control" />
-                        <br />
-                        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-                            Confirm Password
-                        </label>
-                        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
-                        <div className="text-center mt-4">
-                            <MDBBtn color="indigo" type="submit">Update</MDBBtn>
-                        </div>
-                    </form>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+        <div id="reset">
+            <form className="reset-form" id="reset-form" onSubmit={handleSubmit}>
+
+                <h1>Reset Password</h1>
+                <div className="">
+                    <label htmlFor="newPassword">New Password</label>
+                    <input onChange={handleChange} value={newPassword} type="password" className="text" id="newPassword" name="newPassword" />
+
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input onChange={handleChange} value={confirmPassword} type="password" className="text" id="confirmPassword" name="confirmPassword" />
+                </div>
+                <button className="btn-reset" >Update Password</button><br />
+            </form>
+        </div>
     );
 };
 
